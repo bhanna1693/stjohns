7e7bde4a467803700ab6df02af0cb08e72513c09
brianhanna <bhanna1693@gmail.com>
2024-01-21 09:29:58 -0500

    refatored for maintainability

--- handlers/stjohns.go
+++ handlers/stjohns.go
@@ -63,17 +63,37 @@ func HomeHandler(e echo.Context) error {
 	return utils.Render(e, home.Home(data))
 }
 
-func ContactHandler(e echo.Context) error {
-	log.Println("Received contact form submission")
+func sendEmailFromStJohnsToStJohns(contactForm models.Contact) error {
 	from := os.Getenv("EMAIL")
-	to := []string{os.Getenv("EMAIL")}
+	to := []string{from}
 	password := os.Getenv("EMAIL_PASSWORD")
 	smtpServer := os.Getenv("SMTP_SERVER")
 	smtpPort := os.Getenv("SMTP_PORT")
-	smtpHost := smtpServer + ":" + smtpPort
-	auth := smtp.PlainAuth("", from, password, smtpServer)
 
 	log.Println("from: " + from + " to: " + to[0] + " password: " + password + " smtpServer: " + smtpServer + " smtpPort: " + smtpPort)
+
+	log.Println(contactForm)
+	subject := "New Message From " + contactForm.Name + " (" + contactForm.Email + ")" + " via stjohns-lutheran-church.com"
+	message := contactForm.Message + "\n\n" + "Sent from: " + contactForm.Email
+
+	return sendEmail(from, password, smtpServer, smtpPort, to, subject, message)
+}
+
+func sendEmail(from, password, smtpServer, smtoPort string, to []string, subject, body string) error {
+	smtpHost := smtpServer + ":" + smtoPort
+	auth := smtp.PlainAuth("", from, password, smtpServer)
+	message := []byte(
+		"From: " + from + "\r\n" +
+			"To: " + to[0] + "\r\n" +
+			"Subject: " + subject + "\r\n" +
+			"\r\n" +
+			body + "\r\n",
+	)
+	return smtp.SendMail(smtpHost, auth, from, to, message)
+}
+
+func ContactHandler(e echo.Context) error {
+	log.Println("Received contact form submission")
 
 	var contactForm models.Contact
 	if err := e.Bind(&contactForm); err != nil {
@@ -82,17 +102,8 @@ func ContactHandler(e echo.Context) error {
 	}
 
 	log.Println(contactForm)
-	subject := "New Message From " + contactForm.Name + " (" + contactForm.Email + ")" + " via stjohns-lutheran-church.com"
-	contactForm.Message = contactForm.Message + "\n\n" + "Sent from: " + contactForm.Email
 
-	message := []byte(
-		"From: " + from + "\r\n" +
-			"To: " + to[0] + "\r\n" +
-			"Subject: " + subject + "\r\n" +
-			"\r\n" +
-			contactForm.Message + "\r\n",
-	)
-	if err := smtp.SendMail(smtpHost, auth, from, to, message); err != nil {
+	if err := sendEmailFromStJohnsToStJohns(contactForm); err != nil {
 		log.Println(err)
 		// e.Response().Writer.WriteHeader(500)
 		// e.Response().Writer.Write([]byte("Failed to send message"))
@@ -102,7 +113,7 @@ func ContactHandler(e echo.Context) error {
 			Message: "Failed to send message",
 		}))
 	}
-	log.Println("Message sent successfully to " + to[0])
+	log.Println("Message sent successfully")
 	return utils.Render(e, snackbar.Alert(models.Alert{
 		Type:    "success",
 		Message: "Message sent successfully",
